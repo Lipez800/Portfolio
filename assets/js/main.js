99e79da089d48591937938386f5aec39482acf72
Lipez800 <80587876+Lipez800@users.noreply.github.com>
2023-04-11 04:40:42 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,23 +43,28 @@
   /**
    * Navbar links active state on scroll
    */
-  let navbarlinks = select("#navbar .scrollto", true);
+  const navbarlinks = select("#navbar .scrollto", true);
+  const sections = select("section", true);
+
   const navbarlinksActive = () => {
-    let position = window.scrollY + 200;
-    navbarlinks.forEach((navbarlink) => {
-      if (!navbarlink.hash) return;
-      let section = select(navbarlink.hash);
-      if (!section) return;
-      if (
-        position >= section.offsetTop &&
-        position <= section.offsetTop + section.offsetHeight
-      ) {
-        navbarlink.classList.add("active");
+    const currentPosition = window.pageYOffset - 1;
+
+    sections.forEach((section, index) => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.offsetHeight;
+      const sectionBottom = sectionTop + sectionHeight;
+
+      if (currentPosition >= sectionTop && currentPosition <= sectionBottom) {
+        navbarlinks.forEach((navbarlink) => {
+          navbarlink.classList.remove("active");
+        });
+        navbarlinks[index].classList.add("active");
       } else {
-        navbarlink.classList.remove("active");
+        navbarlinks[index].classList.remove("active");
       }
     });
   };
+
   window.addEventListener("load", navbarlinksActive);
   onscroll(document, navbarlinksActive);
 
